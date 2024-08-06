904574ae878d9e92fe313ddb4ef4d64a2e8eb685
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2024-08-05 21:38:28 -0700

    Add command line tool for simulation

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from simulator import Simulator
 from agents import GreedyAgent, RandomAgent
+import click
 
-def main():
+implemented_agents = ['greedy', 'random']
+agent_map = {
+    'greedy': GreedyAgent(),
+    'random': RandomAgent()
+}
+@click.command()
+@click.option("-n", default=50000, help="Number of games to simulate")
+@click.option("-a",
+                default="greedy",
+                help="Agent A",
+                type=click.Choice(implemented_agents))
+@click.option("-b",
+                default="random",
+                help="Agent B",
+                type=click.Choice(implemented_agents))
+def simulate(n, a, b):
     sim = Simulator()
-    a_wins, ties, b_wins = sim.simulateNGames(GreedyAgent(), RandomAgent(), 80000)
+    agent_a = agent_map[a]
+    agent_b = agent_map[b]
+    a_wins, ties, b_wins = sim.simulateNGames(agent_a, agent_b, n_games=n)
 
     print(f"Results: A {a_wins} - {b_wins} B")
     print(f"A win rate: {"{:.2f}".format(a_wins / (a_wins + b_wins) * 100)}%")
 
 
 if __name__ == "__main__":
-    main()
+    simulate()
